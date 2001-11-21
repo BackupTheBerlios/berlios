2b7c1f0cc3e8a88e4e16e32fbbc9852ddb22ad8b
riessen <riessen>
2001-11-21 16:36:50 +0000

    aargh!

--- berlios/re_create_links.sh
+++ berlios/re_create_links.sh
@@ -8,15 +8,18 @@
 #
 # Author: Gerrit Riessen
 #
+TOP_LEVEL=`pwd`
+
 ln -s index.php.de index.php
 ln -s berlios.php berlios.css
-cd about
+cd $TOP_LEVEL/about
 ln -s ../include include
-cd ../contact
+cd $TOP_LEVEL/contact
 ln -s ../include include
-cd partners/include
+cd $TOP_LEVEL/partners/include
 ln -s ../../include/backend2newslist.php backend2newslist.php
 
+cd $TOP_LEVEL
 # the following assume external file structures
 ln -s ../phpAdsNew_2beta5 phpAdsNew
 ln -s ../phpMyAdmin-2.2.0 phpMyAdmin
