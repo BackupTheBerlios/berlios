f5f1f7ae2ba5aae373f8562d82fee492324b519a
helix <helix>
2004-02-27 14:41:26 +0000

    add Google Ads

---
+++ berlios/include/adsense.php
@@ -0,0 +1,19 @@
+<?php
+echo '
+<center>
+<script type="text/javascript"><!--
+google_ad_client = "pub-3290644696617321";
+google_ad_width = 468;
+google_ad_height = 60;
+google_ad_format = "468x60_as";
+google_color_border = "CCCCCC";
+google_color_bg = "FFFFFF";
+google_color_link = "000000";
+google_color_url = "666666";
+google_color_text = "333333";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</center>';
+?>
