d65b72e084c37871269b1f9326476f964e76a88b
helix <helix>
2004-09-02 10:35:10 +0000

    Does not create or delete FTP projekt directory if not in use

--- SF2.5/backend/berlios/create_project_ftpdir.pl
+++ SF2.5/backend/berlios/create_project_ftpdir.pl
@@ -11,34 +11,41 @@
 require("include.pl");  # Include all the predefined functions and variables
 
 my @group_dump = open_array_file($config{'group_dump'});
-my ($groupname, $status, $groupid, $userlist);
+my ($groupname, $status, $groupid, $userlist, $use_cvs, $use_svn, $use_ftp);
 
 print("\nCreating FTP Directories for Projects/Groups\n");
 while ($ln = shift(@group_dump)) {
-  chop($ln);
-  ($groupname, $status, $groupid, $userlist) = split(":", $ln);
-  $ftpdir = $config{'project_ftp'}."/".$groupname;
-  if ($status eq "A") {
-	if ( ! -d $ftpdir ) {
-		print("mkdir -m 0775 $ftpdir\n");
-		system("mkdir -m 0775 $ftpdir");
+	chop($ln);
+	($groupname, $status, $groupid, $userlist, $use_cvs, $use_svn, $use_ftp) = split(":", $ln);
+	$ftpdir = $config{'project_ftp'}."/".$groupname;
+	if ($status eq "A") {
+		if ($use_ftp) {
+			if ( ! -d $ftpdir ) {
+				print("mkdir -m 0775 $ftpdir\n");
+				system("mkdir -m 0775 $ftpdir");
+			}
+			print("chmod g+s $ftpdir\n");
+			system("find $ftpdir -type d | xargs chmod g+s");
+			print("chown $groupname:$groupname $ftpdir\n");
+			system("chown $groupname:$groupname $ftpdir");
+        		print("chgrp -R $groupname $ftpdir\n");
+        		system("chgrp -R $groupname $ftpdir");
+        		print("chmod -R a+r $ftpdir\n");
+        		system("chmod -R a+r $ftpdir");
+			print("chmod -R ug+w $ftpdir\n");
+			system("chmod -R ug+w $ftpdir");
+		} else {
+			if ( -d $ftpdir ) {
+				print("rm -r $ftpdir\n");
+				system("rm -r $ftpdir");
+			}
+		}
+	} elsif ($status eq "D") {
+		if ( -d $ftpdir ) {
+			print("rm -r $ftpdir\n");
+			system("rm -r $ftpdir");
+		}
 	}
-	print("chmod g+s $ftpdir\n");
-	system("find $ftpdir -type d | xargs chmod g+s");
-	print("chown $groupname:$groupname $ftpdir\n");
-	system("chown $groupname:$groupname $ftpdir");
-        print("chgrp -R $groupname $ftpdir\n");
-        system("chgrp -R $groupname $ftpdir");
-        print("chmod -R a+r $ftpdir\n");
-        system("chmod -R a+r $ftpdir");
-	print("chmod -R ug+w $ftpdir\n");
-	system("chmod -R ug+w $ftpdir");
-  } elsif ($status eq "D") {
-	if ( -d $ftpdir ) {
-		print("rm -r $ftpdir\n");
-		system("rm -r $ftpdir");
-	}
-  }
 }
 
 print ("Done\n");
