8d75a8516cddb5f51a9ce2f523049ade721a0f74
helix <helix>
2004-10-11 14:52:39 +0000

    Correct permissions for usage subdir

--- SF2.5/backend/shell/portals_perm.sh
+++ SF2.5/backend/shell/portals_perm.sh
@@ -3,17 +3,19 @@
 echo ""
 echo "Set permissions for portals: "
 
-logdir=/var/log/httpd
 htdir=/usr/local/httpd/htdocs
 
-for i in berlios devel forum news wiki ; do
-        echo "Set owner and permissions for usage directory of $i"
-	chown berlios:berlios $htdir.$i/usage
-	chmod 0775 $htdir.$i/usage
+for i in berlios devel forum news ; do
+	echo "Set owner and permissions for usage directory of $i"
+	chown helix:berlios $htdir.$i/usage
+	chmod 0755 $htdir.$i/usage
+	chmod g+s $htdir.$i/usage
 
 	echo "Set owner and permissions for usage directory files of $i"
-	chown berlios:berlios $htdir.$i/usage/*
-	chmod 0664 $htdir.$i/usage/*
+	chown helix:berlios $htdir.$i/usage/*
+	chmod 0644 $htdir.$i/usage/*
+	chown helix:berlios $htdir.$i/usage/.htaccess
+	chmod 0644 $htdir.$i/usage/.htaccess
 
 done
 
