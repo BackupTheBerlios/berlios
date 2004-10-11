d8c915bc8a6068456d57f94b35defb52ac44e784
helix <helix>
2004-10-11 14:56:18 +0000

    Add vhosts

--- SF2.5/backend/shell/webalizer_new.sh
+++ SF2.5/backend/shell/webalizer_new.sh
@@ -9,7 +9,7 @@ for i in `cd /home/groups ; ls | grep -v lost+found | grep -v quota.group | grep
 	if [ ! -d /home/groups/$i/htdocs/usage ] ; then
 		mkdir /home/groups/$i/htdocs/usage
 		chown $i:$i /home/groups/$i/htdocs/usage
-		chmod 0775 /home/groups/$i/htdocs/usage
+		chmod 0755 /home/groups/$i/htdocs/usage
 		chmod g+s /home/groups/$i/htdocs/usage
 	fi
 
@@ -17,25 +17,31 @@ for i in `cd /home/groups ; ls | grep -v lost+found | grep -v quota.group | grep
 		webalizer -p -n "$i.berlios.de" -o /home/groups/$i/htdocs/usage /home/groups/$i/log/combined.$l
 	done
 
-        for k in `cd /home/groups/$i/vhost ; ls` ; do
-		if [ ! -d /home/groups/$i/vhost/$k/htdocs/usage ] ; then
-                	mkdir /home/groups/$i/vhost/$k/htdocs/usage
-                	chown $i:$i /home/groups/$i/vhost/$k/htdocs/usage
-                	chmod 0775 /home/groups/$i/vhost/$k/htdocs/usage
-                	chmod g+s /home/groups/$i/vhost/$k/htdocs/usage
-        	fi
+	if [ -d /home/groups/$i/vhost ] ; then
+        	for k in `cd /home/groups/$i/vhost ; ls` ; do
+			if [ ! -d /home/groups/$i/vhost/$k/htdocs/usage ] ; then
+                		mkdir /home/groups/$i/vhost/$k/htdocs/usage
+                		chown $i:$i /home/groups/$i/vhost/$k/htdocs/usage
+                		chmod 0755 /home/groups/$i/vhost/$k/htdocs/usage
+                		chmod g+s /home/groups/$i/vhost/$k/htdocs/usage
+        		fi
 
-        	for l in `cd /home/groups/$i/vhost/$k/log ; ls 2*.log` ; do
-            		webalizer -p -n "$k" -o /home/groups/$i/vhost/$k/htdocs/usage /home/groups/$i/vhost/$k/log/combined.$l
-        	done
+        		for l in `cd /home/groups/$i/vhost/$k/log ; ls 2*.log` ; do
+            			webalizer -p -n "$k" -o /home/groups/$i/vhost/$k/htdocs/usage /home/groups/$i/vhost/$k/log/combined.$l
+        		done
 
-		chown $i:$i /home/groups/$i/vhost/$k/htdocs/usage/*
-		chmod 0664 /home/groups/$i/vhost/$k/htdocs/usage/*
+			chown $i:$i /home/groups/$i/vhost/$k/htdocs/usage/*
+			chmod 0644 /home/groups/$i/vhost/$k/htdocs/usage/*
 
-	done
+			find /home/groups/$i/vhost/$k/htdocs/usage -mtime +366 -a -type f -exec rm {} \;
+
+		done
+	fi
 
 	chown $i:$i /home/groups/$i/htdocs/usage/*
-	chmod 0664 /home/groups/$i/htdocs/usage/*
+	chmod 0644 /home/groups/$i/htdocs/usage/*
+
+        find /home/groups/$i/htdocs/usage -mtime +366 -a -type f -exec rm {} \;
 
 done
 
